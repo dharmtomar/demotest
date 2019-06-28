e62c3476f0f586d11b16737e12467aa1ac0c72b8
dharmtomar <dtdtomar2@gmail.com>
2019-06-28 16:57:48 +0530

    add sim

--- demotest/src/main/java/regression_ObjectPages/AddSim.java
+++ demotest/src/main/java/regression_ObjectPages/AddSim.java
@@ -1,5 +1,8 @@
 package regression_ObjectPages;
 
+import java.io.IOException;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 import utills.CommonMethods;
@@ -8,5 +11,8 @@ public class AddSim extends CommonMethods{
 	public WebDriver odriver;
 	public AddSim(WebDriver odriver) {
 		this.odriver=odriver;
+	}
+	public void addsim() throws IOException{
+		click_element(odriver, By.xpath(getlocvalue("addsimlink")), 15);
 	}
 }
